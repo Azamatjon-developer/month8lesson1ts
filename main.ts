6740a9caf74688b2d04606c6e3645dee4734abd6
Azamatjon <azamatergasev01@gmail.com>
2024-09-26 14:13:25 +0500

    3tasks

---
+++ main.ts
@@ -0,0 +1,75 @@
+// 1 task tushunishim bo'yicha o'zgaruvchilarga type berish kerak edi type berdim pastdagi xolatda yoki interface qilib ishlatish kerak bo'lsa deb interface orqali ishlatdim
+
+// let fullName :string = "John Doe";
+// let age:number = 30;
+// let isStudent:boolean = true;
+// let hobbies:string[] = ["Reading", "Gaming", "Coding"];
+//
+//   interface Person {
+//     fullName: string;
+//     age: number;
+//     isStudent: boolean;
+//     hobbies: string[];
+//   }
+//
+//
+//   const Person: Person= {
+//     fullName: "Azamatjon",
+//     age: 22,
+//     isStudent: true,
+//     hobbies: ["Reading", "Cooking", "Coding"]
+// };
+//
+//
+// function greet(person: string): string {
+//     return `Hello, ${person}`;
+//
+//   }
+//   console.log(greet(Person.fullName));
+
+// 2 task array ichida nechta pozitive yani musbat son bor ekanini aniqlab beradi
+// function countPositiveNumbers(arr: number[]) : number {
+// let filteredArr = arr.filter(item => item > 0);
+// let sum = filteredArr.length
+// return sum;
+// }
+// let arr = [1, -2, 3, -4, 5, -6, 7, -8, 9, -10,12,3];
+// console.log(countPositiveNumbers(arr));
+
+
+// 3 task agar kiritilgan son 3 ga qoldiqsiz bo'linsa fizz agar 5 ga qoldiqsiz bo'linsa buzz agar   3 ga qoldiqsiz  va 5 ga qoldiqsiz bo'linsa fizzbuzz ko'rsatadi
+
+// function fizzBuzz(n: number){
+//     if (n % 3 === 0 && n % 5 === 0) {
+//         console.log(`${n} FizzBuzz`);
+//     }else if (n % 3 === 0) {
+//         console.log(`${n} Fizz`);
+//     }else if (n % 5 === 0) {
+//         console.log(`${n} Buzz`);
+//     }else{
+//         console.log(`${n} Not Fizz or Buzz`);
+//     }
+// }
+
+// (fizzBuzz(15));
+
+
+// 4 task array ichida barcha sonlarni yigindisini topadi
+
+// function sumArray(arr: number[]) {
+//     let sum = arr.reduce((a, b) => a + b, 0);
+//     console.log(sum)
+// }
+
+// let arr:number[] = [1, 2, 3, 4, 5,6,7,8,9,10];
+// sumArray(arr);
+
+
+// 5 task 
+
+// function countWords(str) { 
+	// 
+// }
+// 
+// console.log(countWords("Hello World! This is a test."));
+
